8e8ea6e2e20f827c296d088a44bf1693b5876c93
KryptonCaptain <cjalcorn.s446@gmail.com>
2016-07-05 01:31:09 +1000

    Restructure
    Added a bunch of config options and started shifting code to adjust

---
+++ src/main/java/tb/common/event/TBEventHandler.java
@@ -0,0 +1,11 @@
+package tb.common.event;
+
+import tb.common.item.ItemHerobrinesScythe;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent.NameFormat;
+
+public class TBEventHandler {
+	
+
+
+}
